3f54080e7fb8ac06541bc45b7dc24982841c9fbe
Bacar7 <soilihib17@gmail.com>
2023-09-11 15:31:16 +0100

    tes

--- server.py
+++ server.py
@@ -6,13 +6,13 @@ def server_program():
     host = socket.gethostname()
     port = 5000  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
+    maSocket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    maSocket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
-    server_socket.listen(5)
-    conn, address = server_socket.accept()  # accept new connection
+    maSocket.listen(5)
+    conn, address = maSocket.accept()  # accept new connection
     print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
@@ -21,6 +21,8 @@ def server_program():
             # if data is not received break
             break
         print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 

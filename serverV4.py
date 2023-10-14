f0128375c73e3c4eb075249a50832ca0603dcbd9
Bacar7 <144209251+Bacar7@users.noreply.github.com>
2023-10-14 16:13:16 +0100

    Update serverV4.py

--- serverV4.py
+++ serverV4.py
@@ -1,12 +1,37 @@
 import socket
-import threading
-import random #importation de la librairie "random" pour effectuer un choix aléatoire 
-    
+import threading #contient des fonctionnalités pour travailler avec 
+#des threads. permet de créer et de gérer des threads, 
+#utils pour exécuter plusieurs parties d'un programme simultanément.
+import random
 
+# Variables
+clients = 0
+ 
+
+def choisir_niveau(niveau):
+    mots_faciles = ["moto", "avion", "chat", "camion", "lapin", "chien"]
+    mots_moyens = ["ordinateur", "programmation", "éléphant", "intelligence", "algorithme"]
+    mots_difficiles = ["rhinopharyngitolaryngographologiquement", "cyclopentanoperhydrophénanthrène", "hippopotomonstrosesquippedaliophobie"]
+
+    if niveau == "facile":
+        return random.choice(mots_faciles)
+    elif niveau == "moyen":
+        return random.choice(mots_moyens)
+    elif niveau == "difficile":
+        return random.choice(mots_difficiles)
 
 def jeu_pendu(mySocket):
     
-    choix = ["rhinopharyngitolaryngographologiquement", "cyclopentanoperhydrophénanthrène", "voiture", "camion", "moto", "avion", "chat", "chien", "souris", "éléphant", "ordinateur", "téléphone", "tablette", "écouteur"] #variable contenant les mots du jeu à deviner 
+    niveaux = ["facile", "moyen", "difficile"]
+    mySocket.send("\nBienvenue au jeu du pendu ! Vous avez 3 niveaux de difficulté (facile, moyen, difficile): ".encode())
+    niveau_choisi = mySocket.recv(1024).decode().lower()  
+    
+    while niveau_choisi not in niveaux:
+        mySocket.send("Niveau invalide. Fermeture de la connexion.".encode())
+        mySocket.close()
+        return
+
+  
     lettre_trouvée = "" #variable permetant de répertorier les lettres correctes trouvées
     lettre_utilisée = [] #variable permetant de répertorier les lettres utilisées 
     Mot = "" #variable permettant d'afficher au fur et à mesure le mot à deviner 
@@ -14,36 +39,31 @@ def jeu_pendu(mySocket):
     c = True 
     n = True
     j = ""
-    solution = random.choice(choix) #variable contenant le mot à deviner, qui utilise une fonction aléatoire, permettant de choisir un mot aléatoire dans la variable choix
+    solution = choisir_niveau(niveau_choisi) 
     
     tentative = 7
     
     for i in solution :
         Mot = Mot + "_ "
-    #boucle permettant d'afficher des "_" en fonction du nombre de lettre à trouver  
+#boucle permettant d'afficher des "_" en fonction du nombre de lettre à trouver  
 
-    mySocket.send(f"\nBienvenue au jeu du pendu ! Vous avez {tentative} tentatives.\n".encode())
-    #Début du jeu avec la boucle while tant que b est vrai
+#Début du jeu avec la boucle while tant que b est vrai
     while b : 
-        
-        mySocket.send(f"Mot à deviner : {Mot} ".encode()) 
-
         print(f"Le mot à trouver est : {solution}")
+        print ("Mot à deviner :",Mot) #Afficher la variable Mot 
+        mySocket.send(f"Mot à deviner : {Mot}\n".encode())
 
-        mySocket.recv(1024).decode()
-        
         if "_" not in Mot:
-            mySocket.send(f">>> Bravo vous avez trouvé le mot à deviner en {tentative} tentative ! <<<\n".encode())
+            mySocket.send(f">>> Bravo vous avez trouvé le mot à deviner << {solution} >> en {tentative} tentative ! <<<\n".encode())
             print(f">>> Bravo vous avez trouvé le mot à deviner en {tentative} tentative ! <<<")
             break #break permet de sortir directement du programme 
         #condition if permettant de vérifier si la variable Mot ne contient plus de "_". 
         # Si Mot n'a plus de "_", affichage victoire 
-    
+        
         #Boucle permettant de vérifier si l'utilisateur n'utilise pas plusieurs fois la même lettre 
-        while n : #Tant que n vrai 
+        while n : #Tant que n vrai< 
             a = mySocket.recv(1024).decode() #affichage d'un champ permettant à l'utilisateur de choisir une lettre
             print(f"lettre choisi : {a}")
-
 
             if len(lettre_utilisée) == 0 : #conditions if permettant de vérifier si la variable lettre_utilisé est vide 
                 n = False 
@@ -58,54 +78,55 @@ def jeu_pendu(mySocket):
                     else : 
                         j = False 
                         n = False 
-        
+            
 
             if j == True : #condition if permettant d'avertir l'utilisateur qu'il a déjà utilisé cettre lettre 
                 mySocket.send(f"La lettre {a} a déjà été utilisé.\n".encode())
+                print("La lettre",a,"a déjà été utilisé.")
 
         d = len(a) #variable d contenant la longueur de la variable a 
-     
-        if d <= 1: #condition if permettant de vérifier si la variable a est une lettre 
         
+        if d <= 1: #condition if permettant de vérifier si la variable a est une lettre 
+            
             for l in solution : #boucle for permettant de parcourir les lettres dans solution
                 if a == l : #condition if permettant de vérifier si la lettre choisi correspond à une lettre dans le mot à deviner 
                     c = True 
                     break
                 else :
                     c = False 
-                
+                    
             if c == True : #condition if permettant d'afficher que la lettre est correcte si la lettre choisi se trouve dans la variable solution
                 mySocket.send(f" La lettre {a} est correct !\n".encode())
+                print("La lettre",a,"est correct !")
                 lettre_trouvée = lettre_trouvée + " " + a #ajout de la lettre choisi dans la variable lettre_trouvée
-                mySocket.send(lettre_trouvée.encode())
                 mySocket.send(f"Les lettres {lettre_trouvée} sont correct.\n".encode())
+                print("Les lettres",lettre_trouvée,"sont correct.")
                 lettre_utilisée.append(a) #ajout de la lettre choisi dans le tableau lettre_utilisé 
-#voir la ligne juste avant
                 mySocket.send(f"Il vous reste {tentative} tentative.\n".encode())
+                print("Il vous reste",tentative,"tentative.")
                 Mot = "" #variable permettant d'afficher au fur et à mesure le mot à deviner  
-                mySocket.send(Mot.encode())
                 for x in solution: #boucle for permettant de parcourir toutes les lettres dans solution
                     if x in lettre_trouvée : #conditions if permettant de vérifier si les lettres dans la variable lettre_trouvée correspondent aux lettres dans solution
                         Mot += x + " " #remplace les "_" par la lettre trouvée 
                     else:
                         Mot += "_ " #ne remplace rien
-                        # mySocket.send(Mot.encode())
-
-                mySocket.send(Mot.encode())
-                mySocket.send(f"\nVous avez {tentative} tentatives.\n".encode())
 
             else : #condition else permettant d'afficher que la lettre est incorrecte si la lettre choisi ne se trouve pas dans la variable solution
                 mySocket.send(f"La lettre {a} est incorrect !\n".encode())
+                print("La lettre",a,"est incorrect !")
                 lettre_utilisée.append(a) #ajout de la lettre choisi dans le tableau lettre_utilisé
                 tentative = tentative - 1 #on retire une tentative si la lettre est incorrecte 
                 mySocket.send(f"Il vous reste {tentative} tentative.\n".encode())
-        
+                print("Il vous reste",tentative,"tentative.")
+            
             if tentative==0: #condition if permettant d'afficher la défaite, le mot à deviner et le pendu si l'utilisateur n'a plus de tentative 
                 mySocket.send(f"Vous n'avez plus de tentative, vous avez perdu.\n".encode())
+                print("Vous n'avez plus de tentative, vous avez perdu.")
                 mySocket.send(f"Le mot à deviner était, {solution} .\n".encode())
+                print("Le mot à deviner était",solution,".")
                 b = False 
                 mySocket.send(f"==========Y=\n".encode())
-                print("==========Y=")
+                print(" ==========Y= ")
             if tentative<=1: #condition if permettant d'afficher la 6ème partie du pendu
                 mySocket.send(f" ||/       |  \n".encode())
                 print(" ||/       |  ")
@@ -123,37 +144,49 @@ def jeu_pendu(mySocket):
                 print("/||           ")
             if tentative<=6: #condition if permettant d'afficher la 1ème partie du pendu
                 mySocket.send(f"==============\n".encode())
-                print("==============\n")
-    
+                print("==============\n") 
+        
         else : #condition else permettant de vérifier si la variable a est un mot
             if a == solution : #condition if permettant de vérifier si le mot choisi est le mot à deviner 
+                print("Bravo vous avez trouvé le mot à deviner en",tentative, "tentative !")
                 mySocket.send(f"Bravo vous avez trouvé le mot à deviner en {tentative} tentative !\n".encode())
                 b = False
-                break
             else : #condition else permettant de vérifier si le mot choisi est incorrecte 
                 tentative = tentative - 1
+                print("Dommage ce n'est pas le bon mot. Il vous reste",tentative, "tentative.")
                 mySocket.send(f"Dommage ce n'est pas le bon mot. Il vous reste {tentative} tentatives.\n".encode())
         n = True #variable n remit a True pour pouvoir effecture vérifier si l'utilisateur n'utilise pas une lettre déjà utilisée 
 
-
-
-
-
 def main():
+    global clients  # Utilise une variable globale pour suivre le nombre de clients connectés
     host = socket.gethostname()
     port = 12345
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
+    server_socket = socket.socket()  # Crée un socket pour le serveur
+    server_socket.bind((host, port))  # Attache l'adresse de l'hôte et le port au socket
+    server_socket.listen(3)  # Limite à 2 clients simultanés
 
+    # "f" indique à Python d'évaluer les expressions à l'intérieur des accolades {} et de les remplacer par leurs valeurs respectives.
     print(f"Le serveur écoute sur le port {port}...")
 
-
     while True:
-        client_socket, addr = server_socket.accept()
-        client_pendu = threading.Thread(target=jeu_pendu, args=(client_socket,))
-        client_pendu.start()
+        client_socket, addr = server_socket.accept()  # Accepte une nouvelle connexion cliente
+
+        if clients < 3:  # Vérifie s'il y a de la place pour un nouveau client
+            clients += 1  # Incrémente le nombre de clients connectés
+            
+            #threads: permet au programme d'accomplir plusieurs tâches en même temps,
+            # une équipe travaillant sur un projet. Chaque membre de l'équipe est 
+            # comme un thread, capable de faire une partie du travail en même temps 
+            # que les autres.
+            #crée un nouvel objet de type thread avec la classe Thread du module threading.
+            #target spécifie la fonction que le thread exécutera. 
+            #args spécifie les arguments à passer à la fonction (jeu_pendu)
+            client_pendu = threading.Thread(target=jeu_pendu, args=(client_socket,))
+            client_pendu.start()  # Lance un thread pour gérer le client
+        else:
+            print("Nombre maximal de clients atteint. Refus de la connexion.")
+            client_socket.close()  # Ferme la connexion du client si le nombre maximal est atteint
 
 if __name__ == "__main__":
     main()

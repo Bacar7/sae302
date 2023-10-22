5daf5720f4cca23bb38d1fea8c4678117cf5c195
aaronp974 <aaron.pigree29@gmail.com>
2023-10-22 17:52:23 +0400

    Correction erreur

--- version_beta_pendu/v3.0/serveur_v3.0.py
+++ version_beta_pendu/v3.0/serveur_v3.0.py
@@ -1,5 +1,4 @@
 import socket                   # Importation du module "socket"
-from pendu/jeu_pendu_v7.0 import jeu_pendu 
 
 host = 'localhost'                  # Définition de l'hôte et du port de connexion
 port = 6000
@@ -18,6 +17,5 @@ print("Le serveur est mis en route...")
 while True :                   # Tant que la fonction est vraie
     connexion, adresse = mySocket.accept()                  # Le serveur établit la connexion et reste connecter
     print("Une personne s'est connectée avec l'adresse IP {0} et sur le port {1}".format(adresse[0], adresse[1]))                    # Affiche un log de connexions clientes
-    game = jeu_pendu()
     connexion.send(game)
 
